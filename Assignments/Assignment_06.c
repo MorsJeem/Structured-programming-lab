7cddda9fe54253e498988883e12f08427c8c3a50
MorsJeem <morsaline12025@gmail.com>
2022-06-04 23:16:23 +0600

    Assignment's problems

---
+++ Assignments/Assignment_06.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+
+    int n, sum = 0;
+    printf("Input Value of terms : ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            sum += i;
+        }
+    }
+
+    printf("sum of the even numbers: %d\n", sum);
+
+    return 0;
+}
+
+// output:
+// Input Value of terms : 6
+// sum of the even numbers: 12
