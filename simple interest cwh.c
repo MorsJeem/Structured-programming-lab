09be2f2af86b5ad2e774d368d14f922c721d8a12
MorsJeem <morsaline12025@gmail.com>
2022-06-04 22:03:41 +0600

    New file added

---
+++ simple interest cwh.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    /*int num,num2,num3;
+
+
+    printf("Please Enter the amount of money:");
+    scanf("%d",&num);
+
+    printf("Please enter the rate of interest:");
+    scanf("%d",num2);
+
+    printf("Please enter the number of years:");
+    scanf("%d",&num3);
+
+    printf("The simple interest of your money will be:%d",(num*num2*num3)/100);*/
+
+    int principal=1000,rate=12,years=10;
+    int simpleinterest=(principal * rate* years)/100;
+
+    printf("The value of simple interst is %d",simpleinterest);
+
+
+    return 0;
+
+
+
+
+
+
+
+}
